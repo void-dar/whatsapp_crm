1ce9c5eff5be28fdc625ecfc72daa2d55593abeb
Void <ugonwamadueke2006@gmail.com>
2025-06-05 14:18:16 +0100

    small changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from app.main import router as whatsapp_router
+from app.router import router as whatsapp_router
 
 app = FastAPI(title="WhatsApp CRM")
 
-app.include_router(whatsapp_router, prefix="api/", tags=["Whatsapp CRM Manager"])
+app.include_router(whatsapp_router, prefix="/api", tags=["Whatsapp CRM Manager"])
